95354267ca5f415d46c54b372c3282633c41e4a1
egopark <egoparkgrau@gmail.com>
2015-12-01 15:38:06 +0100

    Write every 15 minutes

--- jeelink/jeelinkreader.py
+++ jeelink/jeelinkreader.py
@@ -1,6 +1,7 @@
 #!usr/bin/env python
-import serial, sys
-from datetime import datetime
+import serial, sys, os
+from datetime import datetime, timedelta
+
 ser = serial.Serial('/dev/ttyUSB0', 57600)
 
 while 1:
@@ -16,6 +17,10 @@ while 1:
       humidity = int(data[6]) & 0x7F
       bat_low = (int(data[6]) & 0x80) >> 7
       filename = "t_h_"+ str(sensor_node_address)+".log"
-      f = open(filename, 'a')
-      f.write((str(datetime.now()) + " " + str(temperature) + " " + str(humidity) +"\r\n"
-      f.close()
+      last_modified = datetime.fromtimestamp(os.stat(filename).st_mtime)
+      now = datetime.today()
+      delay = timedelta (minutes = 15)
+      if now - last_modified > delay:
+        f = open(filename, 'a')
+        f.write(str(datetime.now()) + " " + str(temperature) + " " + str(humidity) +"\r\n")
+        f.close()
