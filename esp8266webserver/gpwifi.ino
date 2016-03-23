3cf7177d928918e0733e31c749090195e02a613c
egopark <egoparkgrau@gmail.com>
2016-03-23 11:42:05 +0100

    new WiFiHandleClient()

--- esp8266webserver/gpwifi.ino
+++ esp8266webserver/gpwifi.ino
@@ -11,7 +11,9 @@ IPAddress ipSubnet    (255, 255, 255,   0);
 IPAddress ipDNS       (192, 168,   0,   1);
 
 WiFiServer httpServer(portNumber);
-
+WiFiClient client;
+boolean clientConnected = false;
+unsigned long timeOfConnect = 0;
 
 void WiFiStart()
 {
@@ -40,9 +42,50 @@ boolean WiFiIsCONNECTED()
 
 void WiFiHandleClient()
 {
-  WiFiClient client = httpServer.available();
-  if (client) 
+  if (!clientConnected)
   {
-    //TBD
+    client = httpServer.available();
+    if (client) 
+    {
+      // new client
+      clientConnected = true;
+      timeOfConnect = millis();
+    } else {
+      clientConnected = false;
+      timeOfConnect = 0;
+    }
   }
+
+  if (clientConnected && client.available())
+  {
+    String request = client.readStringUntil('\r');
+    client.flush();
+    // 
+    String response;
+    String responseHeader;
+    String responseContent="<html><head><title>test title</title></head><body>test response</body></html>";
+
+    responseHeader  = "HTTP/1.1 200 OK\r\n";
+    responseHeader += "Content-Length: ";
+    responseHeader += responseContent.length();
+    responseHeader += "\r\n";
+    responseHeader += "Content-Type: text/html\r\n";
+    responseHeader += "Connection: close\r\n";
+    responseHeader += "\r\n";
+
+    response  = responseHeader ;
+    response += responseContent;
+    client.print(response);
+    client.stop();
+    clientConnected = false;
+    timeOfConnect = 0;
+  }
+
+  if (clientConnected && ( (timeOfConnect + 500) > millis() ))
+  {
+    client.stop();
+    clientConnected = false;
+    timeOfConnect = 0;
+  }
+
 }
