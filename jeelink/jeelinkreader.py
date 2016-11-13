dfc3bea3a968c4ddb8225a9ad0b6783fcdd23eb2
egopark <egoparkgrau@gmail.com>
2016-11-13 12:19:59 +0100

    add file exists

--- jeelink/jeelinkreader.py
+++ jeelink/jeelinkreader.py
@@ -1,26 +1,33 @@
 #!usr/bin/env python
 import serial, sys, os
 from datetime import datetime, timedelta
+usbDevice = '/dev/ttyUSB0'
 
-ser = serial.Serial('/dev/ttyUSB0', 57600)
-
-while 1:
-  line = ser.readline()
-  data = line.split()
-  if len(data)>2:
-    if ((data[0]=="OK") and (data[1]=="9")):
-      sensor_node_address = data[2]
-      battery = (int(data[3]) & 0x80) >> 7
-      sensor_type = (int(data[3]) & 0x70) >> 4
-      channel = (int(data[3]) & 0x0F)
-      temperature = int(data[4])*256+int(data[5])-1000
-      humidity = int(data[6]) & 0x7F
-      bat_low = (int(data[6]) & 0x80) >> 7
-      filename = "t_h_"+ str(sensor_node_address)+".log"
-      last_modified = datetime.fromtimestamp(os.stat(filename).st_mtime)
-      now = datetime.today()
-      delay = timedelta (minutes = 15)
-      if now - last_modified > delay:
-        f = open(filename, 'a')
-        f.write(str(datetime.now()) + " " + str(temperature) + " " + str(humidity) +"\r\n")
-        f.close()
+if os.path.exists(usbDevice):
+  ser = serial.Serial(usbDevice, 57600)
+  while 1:
+    line = ser.readline()
+    data = line.split()
+    if len(data)>2:
+      if ((data[0]=="OK") and (data[1]=="9")):
+        sensor_node_address = data[2]
+        battery = (int(data[3]) & 0x80) >> 7
+        sensor_type = (int(data[3]) & 0x70) >> 4
+        channel = (int(data[3]) & 0x0F)
+        temperature = int(data[4])*256+int(data[5])-1000
+        humidity = int(data[6]) & 0x7F
+        bat_low = (int(data[6]) & 0x80) >> 7
+        filename = "t_" +datetime.now().strftime("%y_%m_%d_n") + str(sensor_node_address)+".log"
+        if os.path.exists(filename):
+          mode = 'a'
+          last_modified = datetime.fromtimestamp(os.stat(filename).st_mtime)
+          now = datetime.today()
+          delay = timedelta (minutes = 15)
+          writeCondition = ((now - last_modified) > delay)
+        else:
+          mode = 'w'
+          writeCondition = True
+        if writeCondition:
+          f = open(filename, mode)
+          f.write(str(datetime.now().strftime("%H %M")) + " " + str(temperature) + " " + str(humidity) +"\r\n")
+          f.close()
